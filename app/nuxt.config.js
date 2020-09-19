3d70dac0046645132585b094ffc5a40373604ff1
Alex Gronmark <alex.gronmark@wemakewebsites.com>
2020-09-19 15:26:34 +0100

    Setting up working Nuxt environment with certs

---
+++ app/nuxt.config.js
@@ -0,0 +1,54 @@
+import path from 'path'
+import fs from 'fs'
+
+const isDev = process.env.NODE_ENV !== 'production'
+const serverConfig = isDev ? {
+  key: fs.readFileSync(path.resolve(__dirname, 'server.key')),
+  cert: fs.readFileSync(path.resolve(__dirname, 'server.cert'))
+} : {}
+
+console.log(`isDev: ${isDev}`)
+console.log(`serverConfig: ${serverConfig}`)
+
+export default {
+  server: {
+    https: serverConfig
+  },
+  // Global page headers (https://go.nuxtjs.dev/config-head)
+  head: {
+    title: 'app',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  // Global CSS (https://go.nuxtjs.dev/config-css)
+  css: [
+  ],
+
+  // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
+  plugins: [
+  ],
+
+  // Auto import components (https://go.nuxtjs.dev/config-components)
+  components: true,
+
+  // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
+  buildModules: [
+    // https://go.nuxtjs.dev/tailwindcss
+    '@nuxtjs/tailwindcss',
+  ],
+
+  // Modules (https://go.nuxtjs.dev/config-modules)
+  modules: [
+  ],
+
+  // Build Configuration (https://go.nuxtjs.dev/config-build)
+  build: {
+  }
+}
